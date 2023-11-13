4d4d78c06f62aaab95e251a43eb5589ead32c746
cris <crispusgikonyo@gmail.com>
2023-11-13 08:54:39 +0300

    request

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -13,6 +13,44 @@ const cardStyle = {
   textAlign: 'center',
 };
 
+const createRequestStyle = {
+  backgroundColor: '#fff',
+  border: '1px solid #ccc',
+  borderRadius: '0.375rem',
+  padding: '1rem',
+  margin: '1rem 0',
+  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+};
+
+const inputContainer = {
+  marginBottom: '1rem',
+};
+
+const labelStyle = {
+  display: 'block',
+  marginBottom: '0.5rem',
+  fontSize: '0.875rem',
+  fontWeight: 'bold',
+};
+
+const inputStyle = {
+  width: '100%',
+  padding: '0.5rem',
+  fontSize: '1rem',
+  borderRadius: '0.25rem',
+  border: '1px solid #ccc',
+};
+
+const buttonStyle = {
+  backgroundColor: '#009cde',
+  color: '#fff',
+  borderRadius: '0.25rem',
+  padding: '0.5rem 1rem',
+  fontSize: '0.875rem',
+  cursor: 'pointer',
+  // Add any additional button styles
+};
+
 const cardHeader = {
   backgroundColor: '#009cde',
   color: '#fff',
@@ -31,23 +69,16 @@ const requestCard = {
   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
 };
 
-const buttonStyle = {
-  backgroundColor: '#009cde',
-  color: '#fff',
-  borderRadius: '0.25rem',
-  padding: '0.5rem 1rem',
-  fontSize: '0.875rem',
-  cursor: 'pointer',
-  textDecoration: 'none',
-};
+
 
 const RequestsPage = () => {
   const [receivedResults, setReceivedResults] = useState([]);
+  const [showCreateRequest, setShowCreateRequest] = useState(false);
   const navigate = useNavigate();
+  const [successMessage, setSuccessMessage] = useState('');
   const { setAccessToken } = useUser();
 
   useEffect(() => {
-    // Retrieve the access token from the TokenManager
     const storedAccessToken = TokenManager.getToken();
 
     if (!storedAccessToken) {
@@ -56,7 +87,6 @@ const RequestsPage = () => {
     }
     setAccessToken(storedAccessToken);
 
-    // Make a GET request to retrieve user requests
     fetch('https://100088.pythonanywhere.com/api/wallet/v1/user-request', {
       method: 'GET',
       headers: {
@@ -73,23 +103,81 @@ const RequestsPage = () => {
       .then((data) => setReceivedResults(data.data))
       .catch((error) => {
         console.error(error);
-        // Handle the error or show an error message to the user
       });
   }, [navigate, setAccessToken]);
 
   const handleConfirm = (requestId) => {
-    // Implement the logic to confirm the request with the given ID
-    console.log(`Confirmed request with ID: ${requestId}`);
+    const storedAccessToken = TokenManager.getToken();
+
+    fetch('https://100088.pythonanywhere.com/api/wallet/v1/accept-request', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${storedAccessToken}`,
+      },
+      body: JSON.stringify({
+        custom_id: requestId,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setSuccessMessage('Request confirmed!');
+          // Reload the page or update the state as needed
+        } else {
+          throw new Error('Failed to confirm request');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   const handleCancel = (requestId) => {
-    // Implement the logic to cancel the request with the given ID
     console.log(`Cancelled request with ID: ${requestId}`);
+  };
+
+  const handleCreateRequestClick = () => {
+    setShowCreateRequest(true);
+  };
+
+  const handleCloseCreateRequest = () => {
+    setShowCreateRequest(false);
+  };
+
+  const handleRequest = () => {
+    const accountNumber = document.getElementById('accountNumber').value;
+    const amount = document.getElementById('amount').value;
+
+    const storedAccessToken = TokenManager.getToken();
+
+    fetch('https://100088.pythonanywhere.com/api/wallet/v1/request', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${storedAccessToken}`,
+      },
+      body: JSON.stringify({
+        account_no: accountNumber,
+        amount: amount,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setSuccessMessage('Request successful!');
+          // Reload the page
+        } else {
+          throw new Error('Failed to create request');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
     <div style={cardStyle}>
       <div style={cardHeader}>Received Requests</div>
+      {successMessage && <div style={{ color: 'green', marginBottom: '1rem' }}>{successMessage}</div>}
       {receivedResults.map((result) => (
         <div key={result.id} style={requestCard}>
           <div>
@@ -107,9 +195,42 @@ const RequestsPage = () => {
           </div>
         </div>
       ))}
-      <Link to="/create-request" style={buttonStyle}>
+      <button onClick={handleCreateRequestClick} style={buttonStyle}>
         Create New Request
-      </Link>
+      </button>
+
+      {showCreateRequest && (
+        <div style={createRequestStyle}>
+          <div style={inputContainer}>
+            <label htmlFor="accountNumber" style={labelStyle}>
+              Account Number:
+            </label>
+            <input
+              type="text"
+              id="accountNumber"
+              style={inputStyle}
+              // Add your input field styles
+            />
+          </div>
+          <div style={inputContainer}>
+            <label htmlFor="amount" style={labelStyle}>
+              Amount:
+            </label>
+            <input
+              type="text"
+              id="amount"
+              style={inputStyle}
+              // Add your input field styles
+            />
+          </div>
+          <button onClick={handleCloseCreateRequest} style={buttonStyle}>
+            Close
+          </button>
+          <button onClick={handleRequest} style={buttonStyle}>
+            Request
+          </button>
+        </div>
+      )}
     </div>
   );
 };
